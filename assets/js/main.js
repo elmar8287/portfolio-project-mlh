ce50b6abfc0af1f4ebbd829c482499671508d089
elmar8287 <L-mar@inbox.ru>
2022-07-14 18:11:17 +0400

    Give condition if local storage empty

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,35 +13,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 });
 
-var checkbox = document.getElementById("checkbox");
+let checkbox = document.getElementById("checkbox");
 
 checkbox.addEventListener("change", () => {
-
   var currentTheme = document.documentElement.getAttribute("data-theme");
 
   // Switch between `dark` and `light`
-  var switchToTheme = currentTheme === "dark" ? "light" : "dark";
+  var switchToTheme = currentTheme === "dark" ? "light" : "dark"
 
   // Set our currenet theme to the new one
   document.documentElement.setAttribute("data-theme", switchToTheme);
 
-  if(current>="15") {
-    switchToTheme = "dark";
-    localStorage.setItem("darkMode", switchToTheme);
-  } else {
-    switchToTheme = "light";
-    localStorage.setItem("darkMode", switchToTheme);
-  }
+  localStorage.setItem("darkMode", switchToTheme);
 });
 
 // The mode switch automatically on page load
 function autoSwitch() {
   var hour = (new Date()).getHours();
-  if (hour >= "9" && hour <= "16") {
-    checkbox = "true";
+  if (hour >= "9" && hour <= "17") {
+    localStorage.setItem("darkMode", "light");
   } else {
-    checkbox ="false";
+    localStorage.setItem("darkMode", "dark");
   }
 }
 
-window.onload = autoSwitch;
+if(localStorage.getItem("darkMode") === null) {
+  window.onload = autoSwitch;
+}
