4707ff8abd59a4726f2ff30ed8544ac9f8fbe2ec
elmar8287 <L-mar@inbox.ru>
2022-07-14 17:48:48 +0400

    Create a function onload with system time condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,20 +13,35 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 });
 
-const checkbox = document.getElementById("checkbox");
+var checkbox = document.getElementById("checkbox");
 
 checkbox.addEventListener("change", () => {
+
   var currentTheme = document.documentElement.getAttribute("data-theme");
 
   // Switch between `dark` and `light`
-  var switchToTheme = currentTheme === "dark" ? "light" : "dark"
+  var switchToTheme = currentTheme === "dark" ? "light" : "dark";
 
   // Set our currenet theme to the new one
   document.documentElement.setAttribute("data-theme", switchToTheme);
 
-  localStorage.setItem("darkMode", switchToTheme);
+  if(current>="15") {
+    switchToTheme = "dark";
+    localStorage.setItem("darkMode", switchToTheme);
+  } else {
+    switchToTheme = "light";
+    localStorage.setItem("darkMode", switchToTheme);
+  }
 });
 
-const autoSwitch = () => {
-  console.log("This is automode of dark/light theme switcher")
+// The mode switch automatically on page load
+function autoSwitch() {
+  var hour = (new Date()).getHours();
+  if (hour >= "9" && hour <= "16") {
+    checkbox = "true";
+  } else {
+    checkbox ="false";
+  }
 }
+
+window.onload = autoSwitch;
